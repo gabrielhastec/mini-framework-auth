9d3983fafc6caec81dfe2f11f0aa26ed01a2dc77
Gabriel Hastec <gabrielhastec.dev@gmail.com>
2025-08-22 12:52:49 -0300

    chore: adicionar setup.py para tornar pacote instalável
    - Informações do autor, versão, descrição e URL do projeto
    - Especificação de Python >=3.8
    - find_packages() para localizar pacotes

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+name='mini_auth',
+version='0.1',
+packages=find_packages(),
+author='Gabriel Rodrigues',
+author_email='gabrielhastec.dev@gmail.com',
+description='Mini Framework de Autenticação em Python',
+url='https://github.com/gabrielhastec/mini-framework-auth',
+classifiers=[
+'Programming Language :: Python :: 3',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.8',
+)
